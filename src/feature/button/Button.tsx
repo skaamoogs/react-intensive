281673e774a5e74bf28d9eee418d41b1062d1b5e
Astafa <astafa98@mail.ru>
2024-02-06 01:08:04 +0300

    added button component

---
+++ src/feature/button/Button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import buttonStyle from "./Button.module.css";
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+}
+
+const Button = ({ children }: ButtonProps, { onClick }: ButtonProps) => {
+  return (
+    <div>
+      <button className={buttonStyle.item} onClick={onClick}>
+        {children}
+      </button>
+    </div>
+  );
+};
+
+export default Button;
