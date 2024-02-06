3df5e19a2b71ab5833fd2bba11c41c9138167e11
Aleksandr Shabanov <shabalexx@yandex.ru>
2024-02-06 20:40:53 +0300

    add directory structure and format files

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
 import App from './App';
+import { store } from './store/store';
 
 test('renders learn react link', () => {
   render(
     <Provider store={store}>
       <App />
-    </Provider>,
+    </Provider>
   );
 
   expect(screen.getByText(/learn/i)).toBeInTheDocument();
