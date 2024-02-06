d487d37a090598ff3838072dc76e7316ede5b486
Astafa <astafa98@mail.ru>
2024-02-06 20:37:36 +0300

    changes button & remove div in botton

--- src/feature/button/Button.tsx
+++ src/feature/button/Button.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import buttonStyle from "./Button.module.css";
 
-interface ButtonProps {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
   onClick?: () => void;
 }
 
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button: React.FC = ({ children, onClick }: ButtonProps) => {
   return (
-    <div>
-      <button className={buttonStyle.item} onClick={onClick}>
-        {children}
-      </button>
-    </div>
+    <button className={buttonStyle.item} onClick={onClick}>
+      {children}
+    </button>
   );
 };
 
