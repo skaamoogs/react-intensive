9d0b4ec606179ae97a85cffd87c6148b38471758
Astafa <astafa98@mail.ru>
2024-02-06 21:03:04 +0300

    'changes button, remove div 2'

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Button from "./feature/button/Button";
 
 function App() {
   return (
@@ -51,6 +52,7 @@ function App() {
           </a>
         </span>
       </header>
+      <Button>"sdssdsd"</Button>
     </div>
   );
 }
--- src/feature/button/Button.tsx
+++ src/feature/button/Button.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { ReactEventHandler } from "react";
 import buttonStyle from "./Button.module.css";
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
-  onClick?: () => void;
+  onClick?: ReactEventHandler;
 }
 
-const Button: React.FC = ({ children, onClick }: ButtonProps) => {
+const Button: React.FC<ButtonProps> = ({ children, onClick }) => {
   return (
     <button className={buttonStyle.item} onClick={onClick}>
       {children}
