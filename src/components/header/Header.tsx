fdd527109bf365cf91603305793f1175e5229fff
Analsituation <analsituation@gmail.com>
2024-02-07 21:42:19 +0500

    Layout styles moved to pages/Layout.module.scss

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,10 @@ import Button from '../button/Button';
 const Header: FC = () => {
   return (
     <header className={styles.header}>
-      <a href='' className={styles.logo}>
+      <a
+        href='https://github.com/skaamoogs/react-intensive'
+        className={styles.logo}
+      >
         <div className={styles.bold}>
           <span className={styles.bounce_letter}>A</span>
           <span>ston</span>
@@ -16,21 +19,19 @@ const Header: FC = () => {
       <nav className={styles.nav}>
         <ul className={styles.menu_list}>
           <li className={styles.menu_item}>
-            <a href='#'>Movies</a>
+            <a href='https://github.com/skaamoogs/react-intensive'>Movies</a>
           </li>
           <li className={styles.menu_item}>
-            <a href='#'>TV series</a>
+            <a href='https://github.com/skaamoogs/react-intensive'>TV series</a>
           </li>
           <li className={styles.menu_item}>
-            <a href='#'>Cartoons</a>
+            <a href='https://github.com/skaamoogs/react-intensive'>Cartoons</a>
           </li>
         </ul>
       </nav>
       <div className={styles.user_block}>
         <Button>Login</Button>
         <Button>Sign Up</Button>
-        {/* <button className={(styles.button, styles.login)}></button>
-        <button className={(styles.button, styles.signup)}></button> */}
       </div>
     </header>
   );
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 import Header from '../components/header/Header';
 
+import styles from './Layout.module.scss';
+
 const Layout: FC = () => {
   return (
-    <div className='container'>
+    <div className={styles.container}>
       <Header />
-      <main className='main'>
+      <main className={styles.main}>
         {/* Maybe a block with search and filters will be here? */}
         {/* <Outlet /> */}
       </main>
