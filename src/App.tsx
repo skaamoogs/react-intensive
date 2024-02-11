d06bae4c2f9f857a95f0d8e5f850348fb0e7a07e
Ilya Astafyev <112830586+astafa98@users.noreply.github.com>
2024-02-11 08:42:45 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import './App.css';
-<<<<<<< HEAD
-import { Counter } from './components/counter/Counter';
-import { FilmCard } from './components/filmcard/FilmCard';
-=======
 import Layout from './pages/Layout';
->>>>>>> 0f8ba00d38a243ac9268cd484592d33074e36b99
 
 function App() {
   return <Layout />;
