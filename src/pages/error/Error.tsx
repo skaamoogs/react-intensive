97546e7e2bc3333892043c0bb9c485fd08683e0f
Aleksandr Shabanov <shabalexx@yandex.ru>
2024-02-12 17:46:39 +0300

    change error page

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,17 +1,35 @@
-import { Link, useRouteError } from 'react-router-dom';
+import { Link, useRouteError, isRouteErrorResponse } from 'react-router-dom';
 import styles from './Error.module.scss';
 import clsx from 'clsx';
 import { Paths } from '../../const';
 import Button from '../../components/button/Button';
 
-const ErrorPage = () => {
+interface IErrorStatus {
+  code?: number;
+  message: string;
+}
+
+export const ErrorPage = () => {
   const error = useRouteError();
-  console.log(error);
+  const errorStatus: IErrorStatus = {
+    message: 'Undexpected Error. Try to refresh the page',
+  };
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      errorStatus.code = 404;
+      errorStatus.message = 'Page Not Found';
+    } else {
+      errorStatus.message = error.data;
+    }
+  }
 
   return (
     <div className={styles.wrapper}>
-      <h1 className={clsx(styles.text, styles.errorCode)}>404</h1>
-      <p className={styles.text}>Page Not Found</p>
+      <h1 className={clsx(styles.text, styles.errorCode)}>
+        {errorStatus.code}
+      </h1>
+      <p className={styles.text}>{errorStatus.message}</p>
       <Link to={Paths.Root} className={styles.link}>
         <Button>Home</Button>
       </Link>
