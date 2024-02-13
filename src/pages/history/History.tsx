377eac9540944e5c51fcf73c6aee1844b6c56f24
Aleksandr Shabanov <shabalexx@yandex.ru>
2024-02-13 22:40:59 +0300

    change style and props of history entry

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -2,55 +2,23 @@ import clsx from 'clsx';
 import styles from './History.module.scss';
 import { Paths } from '../../const';
 import { Link } from 'react-router-dom';
-
-const cards: HistoryCardProps[] = [
-  {
-    searchQuery: 'Чак Норрис',
-    filters: { year: '2011', country: 'США', genre: 'Боевик' },
-  },
-  {
-    searchQuery: 'Чак Норрис',
-    filters: { year: '2011', country: 'США', genre: 'Боевик' },
-  },
-  {
-    searchQuery: 'Чак Норрис',
-    filters: { year: '2011', country: 'США', genre: 'Боевик' },
-  },
-  {
-    searchQuery: 'Чак Норрис',
-    filters: { year: '2011', country: 'США', genre: 'Боевик' },
-  },
-  {
-    searchQuery: 'Чак Норрис',
-    filters: { year: '2011', country: 'США', genre: 'Боевик' },
-  },
-  {
-    searchQuery: 'Чак Норрис',
-    filters: { year: '2011', country: 'США', genre: 'Боевик' },
-  },
-];
-/*   {
-    searchQuery: 'Космос',
-    filters: ['2010-2020', 'Великобритания', 'Фантастика'],
-  },
-  { searchQuery: 'Бернардо Бертолуччи', filters: ['Италия'] },
-  { searchQuery: 'Адам Сэндлер', filters: ['2023', 'Комедия'] },
-  {
-    searchQuery: 'Какой-то очень длинный запрос для проверки UI',
-    filters: ['Австралия', 'Дететктив'],
-  },
-  { searchQuery: 'Космос', filters: {'2010-2020', 'Фантастика'] },
-  { searchQuery: 'Годзилла', filters: ['Япония'] },
-]; */
+import { MovieFilters } from '../../api/types';
+import { entries } from './mockHistoryEntries';
 
 const HistoryPage = () => {
   return (
     <>
-      <h1 className={styles.title}>История запросов</h1>
       <div className={styles.container}>
-        {cards.map((card) => (
-          <HistoryCard {...card} />
-        ))}
+        <h1 className={styles.title}>История запросов</h1>
+        {entries.length ? (
+          <div className={styles.entries}>
+            {entries.map((card) => (
+              <HistoryEntry {...card} />
+            ))}
+          </div>
+        ) : (
+          <p>No entries</p>
+        )}
       </div>
     </>
   );
@@ -58,30 +26,29 @@ const HistoryPage = () => {
 
 export default HistoryPage;
 
-interface HistoryCardProps {
-  searchQuery: string;
-  filters: Record<string, string>;
+export interface HistoryEntryProps {
+  query?: string;
+  filters?: MovieFilters;
   className?: string;
 }
 
 const createSeacrhParams = (params: Record<string, string>): string =>
   new URLSearchParams(params).toString();
 
-const HistoryCard = ({ searchQuery, filters, className }: HistoryCardProps) => {
+const HistoryEntry = ({ query, filters, className }: HistoryEntryProps) => {
+  let params = query ? { query } : filters ? filters : {};
+
   return (
     <Link
       to={{
-        pathname: Paths.Search,
-        search: createSeacrhParams({ query: searchQuery, ...filters }),
+        pathname: query ? Paths.Search : `/catalog`,
+        search: createSeacrhParams({ ...params }),
       }}
     >
-      <div className={clsx(styles.card, className)}>
-        <p className={styles.title}>{searchQuery}</p>
-        <div className={styles.filters}>
-          {Object.values(filters).map((f) => (
-            <p className={styles.filter}>{f}</p>
-          ))}
-        </div>
+      <div className={clsx(styles.entryContainer, className)}>
+        <span className={styles.entry}>
+          {query || (filters && Object.values(filters).join(', '))}
+        </span>
       </div>
     </Link>
   );
