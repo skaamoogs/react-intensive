6d74e58edb9c8e765baa123a8e067150b75de7e8
Aleksandr Shabanov <shabalexx@yandex.ru>
2024-02-13 13:22:19 +0300

    add history page

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -1,5 +1,35 @@
-const HistoryPage = () => {
-  return <div>History</div>;
+import clsx from 'clsx';
+import styles from './History.module.scss';
+
+interface HistoryPageProps {
+  cards: HistoryCardProps[];
+}
+
+const HistoryPage = ({ cards }: HistoryPageProps) => {
+  return (
+    <>
+      {cards.map((card) => (
+        <HistoryCard {...card} />
+      ))}
+    </>
+  );
 };
 
 export default HistoryPage;
+
+interface HistoryCardProps {
+  searchQuery: string;
+  filters: string[];
+  className?: string;
+}
+
+const HistoryCard = ({ searchQuery, filters, className }: HistoryCardProps) => {
+  return (
+    <div className={clsx(styles.card, className)}>
+      <p className={styles.title}>{searchQuery}</p>
+      {filters.map((f) => (
+        <p className={styles.filter}>{f}</p>
+      ))}
+    </div>
+  );
+};
