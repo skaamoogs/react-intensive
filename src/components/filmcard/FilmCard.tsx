b46eabb2d7756e9ef00cca3746ef60000849a3f0
Astafa <astafa98@mail.ru>
2024-02-13 22:56:47 +0300

    added props in card component

--- src/components/filmcard/FilmCard.tsx
+++ src/components/filmcard/FilmCard.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import card from './FilmCard.module.css';
 
-export const FilmCard: React.FC = () => {
+interface FilmCardProps {
+  imageUrl: string;
+  filmName: string;
+  filmStatus: string;
+}
+
+export const FilmCard: React.FC<FilmCardProps> = ({
+  imageUrl,
+  filmName,
+  filmStatus,
+}) => {
   return (
     <div className={card.item}>
       <div className={card.image}>
-        <img
-          className={card.image}
-          src='https://m.media-amazon.com/images/M/MV5BMjE2NDkxNTY2M15BMl5BanBnXkFtZTgwMDc2NzE0MTI@._V1_.jpg'
-        />
+        <img className={card.image} src={imageUrl} alt={filmName} />
       </div>
-      <div className={card.filmName}>
-        John Wick Chapter Two Chapter Two Chapter
-      </div>
-      <div className={card.filmStatus}>Coming Soon</div>
+      <div className={card.filmName}>{filmName}</div>
+      <div className={card.filmStatus}>{filmStatus}</div>
     </div>
   );
 };
