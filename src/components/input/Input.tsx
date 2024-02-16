74af54e8f7b1cb7ecb6de143b29eaf05b594c4c3
Aleksandr Shabanov <shabalexx@yandex.ru>
2024-02-16 08:49:07 +0300

    add event handlers for search bar

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,25 +1,31 @@
-import { FC, HTMLProps } from 'react';
+import { ForwardedRef, InputHTMLAttributes, forwardRef } from 'react';
 import clsx from 'clsx';
 
 import styles from './Input.module.scss';
 
-export interface InputProps extends HTMLProps<HTMLInputElement> {
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
   error?: string;
 }
 
-const Input: FC<InputProps> = ({ className, value, label, error, ...rest }) => {
-  return (
-    <label className={`${styles.container} ${className}`}>
-      {label && <p className={styles.label}>{label}</p>}
-      <input
-        value={value}
-        className={clsx(styles.input, error && styles.input_error)}
-        {...rest}
-      />
-      {error && <p className={styles.error}>{error}</p>}
-    </label>
-  );
-};
+const Input = forwardRef(
+  (
+    { className, value, label, error, ...rest }: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <label className={`${styles.container} ${className}`}>
+        {label && <p className={styles.label}>{label}</p>}
+        <input
+          value={value}
+          className={clsx(styles.input, error && styles.input_error)}
+          {...rest}
+          ref={ref}
+        />
+        {error && <p className={styles.error}>{error}</p>}
+      </label>
+    );
+  }
+);
 
 export default Input;
--- src/components/search-bar/SeacrhBar.tsx
+++ src/components/search-bar/SeacrhBar.tsx
@@ -1,24 +1,53 @@
-import { useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 import Input from '../input/Input';
 import styles from './SearchBar.module.scss';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import { Paths } from '../../const';
 
-interface SearchBarProps {
-  value?: string;
-}
+const queryParamName = 'query';
 
-const SearchBar = ({ value }: SearchBarProps) => {
-  const [search, setSearch] = useState(value);
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const [params] = useSearchParams();
+  const [searchValue, setSearchValue] = useState(
+    params.get(queryParamName) ?? ''
+  );
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = () => {
+    navigate(Paths.Search);
+  };
+
+  const resetInput = () => {
+    setSearchValue('');
+    inputRef.current?.focus();
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  };
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <Input
-        value={search}
-        onChange={(e) => setSearch((e.target as HTMLInputElement).value)}
+        value={searchValue}
+        onChange={handleInputChange}
         type='search'
         className={styles.input}
+        name={queryParamName}
+        ref={inputRef}
       />
-      <button type='submit' className={styles.searchButton}>
-        <img src='search.svg' alt='search' className={styles.searchIcon} />
+      {searchValue && (
+        <button
+          type='reset'
+          className={styles.cancelButton}
+          onClick={resetInput}
+        >
+          <img src='cross.svg' alt='cancel' className={styles.icon} />
+        </button>
+      )}
+      <button type='submit' className={styles.button}>
+        <img src='search.svg' alt='search' className={styles.icon} />
       </button>
     </form>
   );
